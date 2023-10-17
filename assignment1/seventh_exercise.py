c597443c5464bdb8fcadd2ddc0bae297575b489f
mahmoud99salameh <mahmoud99salameh@gmail.com>
2023-10-17 10:47:44 +0300

    file to folder

--- assignment1/seventh_exercise.py
+++ assignment1/seventh_exercise.py
@@ -3,5 +3,14 @@ def stats_measures(ls):
     mean=0
     for i in ls:
         mean+=i
-    return mean/len(ls)
-
+    # return mean/len(ls)
+    #Calculate Median
+    median=float(0)
+    for i in ls:
+        if len(ls)%2==0:
+            median=(ls[(len(ls)//2)]+ls[(len(ls)//2)-1])
+        else:
+            median=(ls[len(ls)//2])
+        # return median
+    #Calculate Mode
+    
