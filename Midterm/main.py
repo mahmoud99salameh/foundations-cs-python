f244f9b9ded5e49c243300f2834994146d9660f6
mahmoud99salameh <mahmoud99salameh@gmail.com>
2023-11-11 12:38:04 +0200

    midterm third commit

--- Midterm/main.py
+++ Midterm/main.py
@@ -3,15 +3,16 @@ import os
 from selenium import webdriver
 
 
-
+browser = webdriver.Safari()
 #open normal tab
 def OpenNewTab():
     webbrowser.open('https://spotify.com', new=2)
 #close tab
-def CloseTab():
+def CloseSafari():
         os.system('TASKKILL /F /IM safari.???')
 #open tab with web driver
 def OpenWithWebDriver():
-      browser = webdriver.Safari()
       browser.get("https://spotify.com")
       
+def CloseTab():
+      browser.close()
