ab271da06e75f8ece087a042b4d247949833e491
mahmoud99salameh <mahmoud99salameh@gmail.com>
2023-11-12 13:18:15 +0200

    6th commit

--- Midterm/main.py
+++ Midterm/main.py
@@ -1,16 +1,47 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+import time 
+chrome_options = Options()
 
-def clean_browser():
-      option= webdriver.ChromeOptions()
-      option.add_argument('--incognito')
-      driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
-                            options=option)
-      return driver
+chrome_options.add_experimental_option("detach", True)
 
-if __name__ == '__main__':
-      print('1')
-      driver = clean_browser()
-      print('2')
-      driver.get('www.google.com')
+driver = webdriver.Chrome(options=chrome_options)
+
+#open new tab
+def OpenTab():
+    driver.get('https://www.google.com')
+
+#close tab
+def CloseTab():
+    driver.close()
+
+#switch between tabs
+def SwitchTabs():
+    driver.switch_to.window(driver.window_handles[1])
+
+#display all tabs
+def DisplayAll():
+    # Open the first tab and navigate to a website
+    driver.get("https://www.google.com")
+
+    # Open a new tab
+    driver.execute_script("window.open('', '_blank');")
+
+    for handle in driver.window_handles:
+        driver.switch_to.window(handle)
+        print(f"Tab Title: {driver.title}, URL: {driver.current_url}")
+
+#nested tabs
+def NestedTabs():
+    # Open the first tab and navigate to a website
+    driver.get("https://www.example.com")
+
+    # Open a new tab and navigate to another website
+    driver.execute_script("window.open('', '_blank');")
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get("https://www.google.com")
+
+    # Open a nested tab within the second tab
+    driver.execute_script("window.open('', '_blank');")
+    driver.switch_to.window(driver.window_handles[2])
+    driver.get("https://www.facebook.com")
