91fbf885e4fbb5bdb5c28d935c916f1eca8dff38
mahmoud99salameh <mahmoud99salameh@gmail.com>
2023-11-24 18:38:06 +0200

    5th commit

--- assignment4/task_manager.py
+++ assignment4/task_manager.py
@@ -6,26 +6,26 @@ class Task:
         self.completed=False
     @property
     def task_id(self):
-        return self.task_id
+        return self._task_id
     
     @task_id.setter
     def task_id(self,task_id):
         self.task_id=task_id
     @property
     def description(self):
-        return self.description
+        return self._description
     @description.setter
     def description(self,description):
         self.description=description
     @property
     def priority(self):
-        return self.priority
+        return self._priority
     @priority.setter
     def priority(self,priorety):
         self.priority=priorety
     @property
     def completed(self):
-        return self.completed
+        return self._completed
     @completed.setter
     def completed(self,completed):
         self.completed=completed
